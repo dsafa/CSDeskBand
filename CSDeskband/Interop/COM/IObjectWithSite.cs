07ae209e27ff84baa7c5455cfffb6fa21fbeb43c
Brandon Chong <brndnchong@gmail.com>
2017-12-18 18:49:40 -0800

    Add interop interfaces and structs

---
+++ CSDeskband/Interop/COM/IObjectWithSite.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace CSDeskband.Interop.COM
+{
+    //https://msdn.microsoft.com/en-us/library/windows/desktop/ms693765(v=vs.85).aspx
+    [ComImport]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    [Guid("FC4801A3-2BA9-11CF-A229-00AA003D7352")]
+    public interface IObjectWithSite
+    {
+        [PreserveSig]
+        void SetSite([In, MarshalAs(UnmanagedType.IUnknown)] Object pUnkSite);
+
+        [PreserveSig]
+        void GetSite(ref Guid riid, [MarshalAs(UnmanagedType.IUnknown)] out Object ppvSite);
+    }
+}
