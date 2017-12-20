eccf6b29576dd62aa48559e44d6e000317e1b663
Brandon Chong <brndnchong@gmail.com>
2017-12-19 19:15:58 -0800

    Add some documentation to deskband options

--- CSDeskband/CSDeskBandOptions.cs
+++ CSDeskband/CSDeskBandOptions.cs
@@ -8,12 +8,39 @@ namespace CSDeskband
 {
     public class CSDeskBandOptions
     {
+        /// <summary>
+        /// If true, the <see cref="ICSDeskBand.Increment"/> value can be used. Otherwise ignored.
+        /// </summary>
         public bool VariableHeight { get; set; } = false;
+
+        /// <summary>
+        /// Gives the deskband a sunked border
+        /// </summary>
         public bool Sunken { get; set; } = false;
+
+        /// <summary>
+        /// If the deskband is fixed, the gripper is not shown when taskbar is editable
+        /// </summary>
         public bool Fixed { get; set; } = false;
+
+        /// <summary>
+        /// Undeletable
+        /// </summary>
         public bool Undeleteable { get; set; } = false;
+
+        /// <summary>
+        /// Always show gripper even when taskbar isnt editable
+        /// </summary>
         public bool AlwaysShowGripper { get; set; } = false;
+
+        /// <summary>
+        /// No margins
+        /// </summary>
         public bool NoMargins { get; set; } = false;
+
+        /// <summary>
+        /// if true, <see cref="ICSDeskBand.Title"/> is shown next to the deskband
+        /// </summary>
         public bool ShowTitle { get; set; } = false;
     }
 }
