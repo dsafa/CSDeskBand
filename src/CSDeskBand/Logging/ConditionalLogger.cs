81e94e81d2ac28163803151cf07951f55ecceea9
Brandon Chong <brndnchong@gmail.com>
2018-12-07 18:47:45 -0800

    Update liblog and logging facilities

---
+++ src/CSDeskBand/Logging/ConditionalLogger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace CSDeskBand.Logging
+{
+    internal class ConditionalLogger : ILog
+    {
+        private ILog _logger;
+
+        public ConditionalLogger(ILog logger)
+        {
+            _logger = logger;
+        }
+
+        public bool Log(LogLevel logLevel, Func<string> messageFunc, Exception exception = null, params object[] formatParameters)
+        {
+            var logged = false;
+            LogInternal(ref logged, logLevel, messageFunc, exception, formatParameters);
+
+            return logged;
+        }
+
+        [Conditional("CSDESKBAND_ENABLE_LOG")]
+        private void LogInternal(ref bool logged, LogLevel logLevel, Func<string> messageFunc, Exception exception = null, params object[] formatParameters)
+        {
+            logged = _logger.Log(logLevel, messageFunc, exception, formatParameters);
+        }
+    }
+}
---
+++ src/CSDeskBand/Logging/LogHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSDeskBand.Logging
+{
+    internal static class LogHelper
+    {
+        static ILog GetLogger(Type type)
+        {
+            return new ConditionalLogger(LogProvider.GetLogger(type));
+        }
+    }
+}
