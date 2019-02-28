df5512c048e10db068aca2bc290b53b2f8b96163
Brandon Chong <brndnchong@gmail.com>
2019-02-28 14:26:18 -0800

    Clean up project files

--- src/CSDeskBand/Properties/AssemblyInfo.cs
+++ src/CSDeskBand/Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
 
 [assembly: InternalsVisibleTo("CSDeskBand.Win, PublicKey=0024000004800000940000000602000000240000525341310004000001000100a9f0b955c338f18350aaaa2021440670a35c136add2658a116385df841b07789cc120326f3f464c3fda2a21557217f7335ba62476084ea159a2229813a4392ddd1a2d249f02fd347f17c6a8a7e98071e39096215ab5aa62186cd41ac1644b2f0c1c2d0ec490d7df9fb58f3d11880a5bb422a45f8b6869e49a096ec921fe988de")]
 [assembly: InternalsVisibleTo("CSDeskBand.Wpf, PublicKey=0024000004800000940000000602000000240000525341310004000001000100a9f0b955c338f18350aaaa2021440670a35c136add2658a116385df841b07789cc120326f3f464c3fda2a21557217f7335ba62476084ea159a2229813a4392ddd1a2d249f02fd347f17c6a8a7e98071e39096215ab5aa62186cd41ac1644b2f0c1c2d0ec490d7df9fb58f3d11880a5bb422a45f8b6869e49a096ec921fe988de")]
